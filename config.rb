bd2b96ee7d1340b9d2d1fa522e6afb732eb3dd1d
Izabel Lima <belica18@gmail.com>
2017-04-10 17:21:40 +0200

    built flat pages. still have to adjust layout

--- config.rb
+++ config.rb
@@ -15,7 +15,7 @@ activate :autoprefixer
 ###
 
 # Per-page layout changes:
-#
+
 # With no layout
 # page "/path/to/file.html", :layout => false
 #
@@ -28,6 +28,9 @@ activate :autoprefixer
 # end
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+data.flats.each do |name, flat|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
